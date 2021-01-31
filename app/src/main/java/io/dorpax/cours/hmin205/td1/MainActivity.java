1c11b12a567ba2285a5de2f73bbf6d2a74bf2bf5
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-01-31 17:51:28 +0100

    Exercice 4 - Base

--- app/src/main/java/io/dorpax/cours/hmin205/td1/MainActivity.java
+++ app/src/main/java/io/dorpax/cours/hmin205/td1/MainActivity.java
@@ -1,13 +1,26 @@
 package io.dorpax.cours.hmin205.td1;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button button;
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.mainbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "Message Bouton 1", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
