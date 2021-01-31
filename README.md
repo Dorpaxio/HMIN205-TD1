ae8b4501bedc3413ab9f67d1c36297b866c71343
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-01-31 17:37:21 +0100

    Exercice 2

---
+++ README.md
@@ -0,0 +1,5 @@
+Exercice 2 :
+
+1) La principale différence structurelle réside dans le fait que cette fois-ci, les strings ne sont pas écris en dur dans le code. Elles sont gérées par le fichier strings.xml, une ressource.
+
+2) Code modifié dans activity_main.xml, MainActivity.java et strings.xml
--- app/src/main/java/io/dorpax/cours/hmin205/td1/MainActivity.java
+++ app/src/main/java/io/dorpax/cours/hmin205/td1/MainActivity.java
@@ -1,5 +1,8 @@
 package io.dorpax.cours.hmin205.td1;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        EditText editText = findViewById(R.id.editTextTextPersonName2);
+        Toast.makeText(this, getString(R.string.hello_word) + " " + editText.getText().toString(),
+                Toast.LENGTH_LONG).show();
     }
 }
